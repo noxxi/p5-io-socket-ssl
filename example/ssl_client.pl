8aaad64a5c51657f1a6e5ae711d8be8a8f87c6c1
Steffen Ullrich <Steffen_Ullrich@genua.de>
2014-08-13 07:54:53 +0200

    update example/ssl_client,ssl_server

--- example/ssl_client.pl
+++ example/ssl_client.pl
@@ -1,53 +1,57 @@
 #
 # a test client for testing IO::Socket::SSL-class's behavior
-# (marko.asplund at kronodoc.fi).
-#
-# $Id: ssl_client.pl,v 1.7 2002/01/04 08:45:12 aspa Exp $.
-#
-
 
 use strict;
+use warnings;
 use IO::Socket::SSL;
+use Getopt::Long qw(:config posix_default bundling);
 
-my ($v_mode, $sock, $buf);
+my ($cert_file,$key_file,$key_pass,$ca,$name,$no_verify);
+GetOptions(
+    'd|debug:i' => \$IO::Socket::SSL::DEBUG,
+    'h|help'    => sub { usage() },
+    'C|cert=s'  => \$cert_file,
+    'K|key=s'   => \$key_file,
+    'P|pass=s'  => \$key_pass,
+    'ca=s'      => \$ca,
+    'name=s'    => \$name,
+    'no-verify' => \$no_verify,
+) or usage("bad option");
 
-if($ARGV[0] eq "DEBUG") { $IO::Socket::SSL::DEBUG = 1; }
-
-# Check to make sure that we were not accidentally run in the wrong
-# directory:
-unless (-d "certs") {
-    if (-d "../certs") {
-	chdir "..";
-    } else {
-	die "Please run this example from the IO::Socket::SSL distribution directory!\n";
-    }
+sub usage {
+    print STDERR "Error: @_\n" if @_;
+    print STDERR <<USAGE;
+Usage: $0 [options] ip:port
+ip:port - where to connect to
+Options:
+  -d|--debug [level]      enable debugging with optional debug level
+  -h|--help               this help
+  -C|--cert  cert-file    file containing optional client certificate
+  -K|--key   key-file     file containing private key to certificate, default cert-file
+  -P|--pass  passphrase   passphrase for private key, default none
+  --ca dir|file           use given dir/file as trusted CA store
+  --name hostname         use hostname for SNI and certificate check
+  --no-verify             don't verify certificate
+USAGE
+    exit(2);
 }
 
-if(!($sock = IO::Socket::SSL->new( PeerAddr => 'localhost',
-				   PeerPort => '9000',
-				   Proto    => 'tcp',
-				   SSL_use_cert => 1,
-				   SSL_verify_mode => 0x01,
-				   SSL_passwd_cb => sub { return "opossum" },
-				 ))) {
-    warn "unable to create socket: ", &IO::Socket::SSL::errstr, "\n";
-    exit(0);
-} else {
-    warn "connect ($sock).\n" if ($IO::Socket::SSL::DEBUG);
-}
+my $addr = shift(@ARGV) or usage("no target address given");
+@ARGV and usage("too much arguments");
+$key_file ||= $cert_file;
 
-# check server cert.
-my ($subject_name, $issuer_name, $cipher);
-if( ref($sock) eq "IO::Socket::SSL") {
-    $subject_name = $sock->peer_certificate("subject");
-    $issuer_name = $sock->peer_certificate("issuer");
-    $cipher = $sock->get_cipher();
-}
-warn "cipher: $cipher.\n", "server cert:\n", 
-    "\t '$subject_name' \n\t '$issuer_name'.\n\n";
+my $cl = IO::Socket::SSL->new(
+    PeerAddr => $addr,
+    $ca ? ( -d $ca ? ( SSL_ca_path => $ca ):( SSL_ca_file => $ca ) ):(),
+    $name ? ( SSL_hostname => $name ):(),
+    $no_verify ? ( SSL_verify_mode => 0 ):(),
+    $cert_file ? (
+	SSL_cert_file => $cert_file,
+	SSL_key_file  => $key_file,
+	defined($key_pass) ? ( SSL_passwd_cb => sub { $key_pass } ):(),
+    ):()
+) or die "failed to connect to $addr: $!,$SSL_ERROR";
 
-my ($buf) = $sock->getlines;
-
-$sock->close();
-
-print "read: '$buf'.\n";
+warn "new SSL connection with cipher=".$cl->get_cipher." version=".$cl->get_sslversion." certificate:\n".
+    "\tsubject=".$cl->peer_certificate('subject')."\n".
+    "\tissuer=".$cl->peer_certificate('issuer')."\n"
--- example/ssl_server.pl
+++ example/ssl_server.pl
@@ -1,70 +1,79 @@
 #
 # a test server for testing IO::Socket::SSL-class's behavior
-# (marko.asplund at kronodoc.fi).
-#
-# $Id: ssl_server.pl,v 1.7 2000/11/08 09:25:21 aspa Exp $.
-#
 
 use strict;
+use warnings;
 use IO::Socket::SSL;
+use Getopt::Long qw(:config posix_default bundling);
 
+my ($cert_file,$key_file,$key_pass,$ca);
+GetOptions(
+    'd|debug:i' => \$IO::Socket::SSL::DEBUG,
+    'h|help'    => sub { usage() },
+    'C|cert=s'  => \$cert_file,
+    'K|key=s'   => \$key_file,
+    'P|pass=s'  => \$key_pass,
+    'ca=s'      => \$ca,
+) or usage("bad option");
 
-my ($sock, $s, $v_mode);
-
-if($ARGV[0] eq "DEBUG") { $IO::Socket::SSL::DEBUG = 1; }
-
-# Check to make sure that we were not accidentally run in the wrong
-# directory:
-unless (-d "certs") {
-    if (-d "../certs") {
-	chdir "..";
-    } else {
-	die "Please run this example from the IO::Socket::SSL distribution directory!\n";
-    }
+sub usage {
+    print STDERR "Error: @_\n" if @_;
+    print STDERR <<USAGE;
+Usage: $0 [options] ip:port
+ip:port - where to listen
+Options:
+  -d|--debug [level]      enable debugging with optional debug level
+  -h|--help               this help
+  -C|--cert  cert-file    file containing certificate
+  -K|--key   key-file     file containing private key, default cert-file
+  -P|--pass  passphrase   passphrase for private key, default none
+  --ca dir|file           use given dir/file as trusted CA store to verify client certificate
+USAGE
+    exit(2);
 }
 
-if(!($sock = IO::Socket::SSL->new( Listen => 5,
-				   LocalAddr => 'localhost',
-				   LocalPort => 9000,
-				   Proto     => 'tcp',
-				   Reuse     => 1,
-				   SSL_verify_mode => 0x01,
-				   SSL_passwd_cb => sub {return "bluebell"},
-				 )) ) {
-    warn "unable to create socket: ", &IO::Socket::SSL::errstr, "\n";
-    exit(0);
-}
-warn "socket created: $sock.\n";
+my $addr = shift(@ARGV) or usage("no listen address given");
+@ARGV and usage("too much arguments");
+$cert_file or usage("no certificate given");
+$key_file ||= $cert_file;
+
+my $ioclass = IO::Socket::SSL->can_ipv6 || 'IO::Socket::INET';
+my $server = $ioclass->new(
+    Listen => 5,
+    LocalAddr => $addr,
+    Reuse => 1,
+) or die "failed to create SSL server at $addr: $!";
+
+my $ctx = IO::Socket::SSL::SSL_Context->new(
+    SSL_server => 1,
+    SSL_cert_file => $cert_file,
+    SSL_key_file  => $key_file,
+    defined($key_pass) ? ( SSL_passwd_cb => sub { $key_pass } ):(),
+    $ca ? (
+	SSL_verify_mode => SSL_VERIFY_PEER,
+	-d $ca ? ( SSL_ca_path => $ca ):( SSL_ca_file => $ca )
+    ):(),
+) or die "cannot create context: $SSL_ERROR";
 
 while (1) {
-  warn "waiting for next connection.\n";
-  
-  while(($s = $sock->accept())) {
-      my ($peer_cert, $subject_name, $issuer_name, $date, $str);
-      
-      if( ! $s ) {
-	  warn "error: ", $sock->errstr, "\n";
-	  next;
-      }
-      
-      warn "connection opened ($s).\n";
-      
-      if( ref($sock) eq "IO::Socket::SSL") {
-	  $subject_name = $s->peer_certificate("subject");
-	  $issuer_name = $s->peer_certificate("issuer");
-      }
-      
-      warn "\t subject: '$subject_name'.\n";
-      warn "\t issuer: '$issuer_name'.\n";
-  
-      my $date = localtime();
-      print $s "my date command says it's: '$date'";
-      close($s);
-      warn "\t connection closed.\n";
-  }
+    warn "waiting for next connection.\n";
+    my $cl = $server->accept or do {
+	warn "failed to accept: $!\n";
+	next;
+    };
+
+    IO::Socket::SSL->start_SSL($cl, SSL_server => 1, SSL_reuse_ctx => $ctx) or do {
+	warn "ssl handshake failed: $SSL_ERROR\n";
+	next;
+    };
+
+    if ( $cl->peer_certificate ) {
+	warn "new SSL connection with client certificate\n".
+	    "\tsubject=".$cl->peer_certificate('subject')."\n".
+	    "\tissuer=".$cl->peer_certificate('issuer')."\n"
+    } else {
+	warn "new SSL connection without client certificate\n"
+    }
+
+    print $cl "connected with cipher=".$cl->get_cipher." version=".$cl->get_sslversion."\n";
 }
-
-
-$sock->close();
-
-warn "loop exited.\n";
